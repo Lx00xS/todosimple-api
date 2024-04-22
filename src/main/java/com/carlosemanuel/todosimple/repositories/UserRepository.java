d5bce8ab44a93e6f97e80abadf8a65fac6df5e90
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2024-04-22 09:39:44 -0300

    Criando repository de usuario

---
+++ src/main/java/com/carlosemanuel/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.carlosemanuel.todosimple.repositories;
+
+import com.carlosemanuel.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
